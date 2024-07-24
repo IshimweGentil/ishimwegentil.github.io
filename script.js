98074d1153a9743f908fd7837029b6291072212f
Ishimwe Gentil <136391379+IshimweGentil@users.noreply.github.com>
2024-07-24 16:37:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+  // CARD ANIMATION
+
+  document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+      const rect = target.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+  }
+}
+
+document.getElementById('card').addEventListener('click', function() {
+  window.location.href = 'https://www.example.com';
+});
